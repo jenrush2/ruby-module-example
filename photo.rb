0701c93a14fb44ee812b489863733c376aa2b110
Jennifer Rush <jennifer.rush2@gmail.com>
2023-01-25 18:32:00 -0700

    Create commentable

--- photo.rb
+++ photo.rb
@@ -1,4 +1,8 @@
+require "./commentable"
+
+
 class Photo
+	include Commentable
 	attr_reader :image_source, :caption, :comments
 
 	def initialize(image_source, caption)
@@ -18,25 +22,25 @@ class Photo
 		puts "-" * 50
 	end
 
-	def add_comment(comment)
-		comments[next_comment_id] = comment
-		puts "Added '#{comment}' for a total of #{total_comments} comments."
-	end
+	# def add_comment(comment)
+	# 	comments[next_comment_id] = comment
+	# 	puts "Added '#{comment}' for a total of #{total_comments} comments."
+	# end
 
-	def remove_comment(id)
-		comments.delete(id)
-		puts "Deleted comment with ID #{id}. Comment count now at #{total_comments}"
-	end
+	# def remove_comment(id)
+	# 	comments.delete(id)
+	# 	puts "Deleted comment with ID #{id}. Comment count now at #{total_comments}"
+	# end
 
-	def next_comment_id
-		if total_comments == 0
-			total_comments + 1
-		else
-			comments.keys.last + 1
-		end
-	end
+	# def next_comment_id
+	# 	if total_comments == 0
+	# 		total_comments + 1
+	# 	else
+	# 		comments.keys.last + 1
+	# 	end
+	# end
 
-	def total_comments
-		comments.count
-	end
+	# def total_comments
+	# 	comments.count
+	# end
 end
